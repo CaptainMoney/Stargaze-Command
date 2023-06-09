3387c22d24ea2808ee3a3562f67854a324049a0d
Justin Werner <33844553+CaptainMoney@users.noreply.github.com>
2023-06-09 14:18:06 -0700

    Update index.js
    This is fun

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Player ship
+const playerShip = {
+  x: canvas.width / 2,
+  y: canvas.height - 50,
+  width: 30,
+  height: 30
+};
+
+// Enemy ships 
+const enemyShips = [];  
+
+function spawnEnemy() {
+  // Spawn enemy at random x position at top of screen
+  // Add to enemyShips array
+}
+
+function update() {
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  // Draw player ship
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(playerShip.x, playerShip.y, playerShip.width, playerShip.height);
+  
+  // Update position of enemy ships and draw them
+  enemyShips.forEach(ship => {
+    ship.y += 5;  
+    ctx.fillRect(ship.x, ship.y, ship.width, ship.height);
+  });
+  
+  // Spawn new enemy every 2 seconds
+  spawnEnemy(); 
+}
+
+setInterval(update, 20);
 
